81be43c82638a4513b60e1ad8101fdc343e299d6
shashank <shashankchaudhary250341@gmail.com>
2023-04-13 13:14:43 +0530

    initial commit

--- src/component/Edit.js
+++ src/component/Edit.js
@@ -1,25 +1,25 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-const Edit = () => {
+function Edit() {
   return (
     <div>Update Student Details
-            <form>
-            <div className='form-div'><label htmlFor="name">Name:</label> <br />
-            <input type="text" /><br /></div>
-            
-            <div className='form-div'><label htmlFor="age">Age</label><br />
-            <input type="number" /><br /></div>
-            
-            <div className='form-div'><label htmlFor="course">Course</label><br />
-            <input type="text" /><br /></div>
-            
-            <div className='form-div'><label htmlFor="batch">Batch</label><br />
-            <input type="text" /></div>
-            
+      <form>
+        <div className='form-div'><label htmlFor="name">Name:</label> <br />
+          <input type="text" /><br /></div>
+
+        <div className='form-div'><label htmlFor="age">Age</label><br />
+          <input type="number" /><br /></div>
+
+        <div className='form-div'><label htmlFor="course">Course</label><br />
+          <input type="text" /><br /></div>
+
+        <div className='form-div'><label htmlFor="batch">Batch</label><br />
+          <input type="text" /></div>
+
         <Link to="/student"><button>Cancel</button></Link>
         <Link to="/student"><button type='button'>Update</button></Link>
-        </form>
+      </form>
 
     </div>
   )
